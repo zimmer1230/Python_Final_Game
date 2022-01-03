758062a99147ffaec0f5ab3d131544e92479c651
zimmer1230 <61425969+zimmer1230@users.noreply.github.com>
2022-01-03 13:56:30 +0800

    add kv

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+class FirstApp(App):
+    pass
+if __name__ == "__main__":
+    FirstApp().run()
